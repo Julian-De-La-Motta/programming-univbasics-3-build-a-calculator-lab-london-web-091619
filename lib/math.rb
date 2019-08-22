9f47eb5b2e2edf63a7342cf87d723b72ed6694af
glorious-communicator-0403 <juliandelamotta@gmail.com>
2019-08-22 14:51:27 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,32 +4,44 @@
 
 
 def addition(num1, num2)
-#puts "adds two numbers together"
-sum=num1+num2
-puts sum
+  #puts "adds two numbers together"
+  sum=num1+num2
+  puts sum
 end
 
 def subtraction(num1, num2)
-#puts "subtracts two numbers from each other"
-minus=num1-num2
-puts minus
+  #puts "subtracts two numbers from each other"
+  minus=num1-num2
+  puts minus
 end
 
 def division(num1, num2)
-
+  puts "divide one number from another"
+  divide = num1/num2
+  puts divide
 end
 
 def multiplication(num1, num2)
-
+  puts "multiplies two numbers together"
+  multiply = num1*num2
+  puts multiply
 end
 
 def modulo(num1, num2)
-
+  puts "returns the remainder of two numbers"
+  mod=num1%num2
+  puts mod
 end
 
 def square_root(num)
-
+  puts "returns the square root of a number"
+  sqroot = Math.sqrt(81)
+  puts sqroot
 end
 
-addition(5, 4)
-subtraction(10, 5)
+addition(5,4)
+subtraction(10,5)
+division(50,2)
+multiplication(4,30)
+modulo(34,5)
+square_root(81)

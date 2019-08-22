84b3ee3b8c6e1320380f6328a55087dbafdac8a3
glorious-communicator-0403 <juliandelamotta@gmail.com>
2019-08-22 14:30:37 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,9 +1,22 @@
-def addition(num1, num2)
+#num = 4
+#num1 = 9
+#num2 = 6
 
+
+def addition(num1, num2)
+#puts "adds two numbers together"
+num1 = 5
+num2 = 4
+sum=num1+num2
+puts sum
 end
 
 def subtraction(num1, num2)
-
+#puts "subtracts two numbers from each other"
+num1 = l0
+num2 = 5
+minus=num1-num2
+puts minus
 end
 
 def division(num1, num2)
